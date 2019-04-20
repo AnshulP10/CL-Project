721245efc633892ce0849dc09cf37dc9aa90aad1
Anshul <anshul.padhi@research.iiit.ac.in>
2019-04-20 22:44:59 +0530

    Script for processing English chunked outputs

---
+++ final inputs/eng_ext.py
@@ -0,0 +1,29 @@
+f = open('eng_chunk.txt', 'r')
+g=open('eng_ext.txt', 'r+')
+final=open('eng_extract.txt', 'r+')
+
+arr = [line for line in f.read().split('\n')]
+
+
+for l in arr:
+    if len(l)==0:
+        with open("eng_ext.txt", "a") as newfile:
+            newfile.write('\n')
+    elif len(l) and l[-1]==')':
+        with open("eng_ext.txt", "a") as newfile:
+            newfile.write(l+'\n')
+
+flag=0
+arr1=[line for line in g.read()]
+for lin in arr1:
+    for c in lin:
+        if c ==' ' and flag==0:
+            final.write(c)
+            flag=1
+        elif c==" " and flag==1:
+            pass
+        elif c!=' ' and flag==1:
+            final.write(c)
+            flag=0
+        else:
+            final.write(c)
